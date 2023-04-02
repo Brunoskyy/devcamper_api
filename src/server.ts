870ed9e361e6bbf8c1ded4a872db4ef503325100
Bruno <arturbrunoferreira@gmail.com>
2023-04-02 10:14:25 -0300

    feat: create logger middleware and use morgan

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
 
-import bootcamps from './routes/bootcamps';
+import bootcamps from "./routes/bootcamps";
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use('/api/v1/bootcamps', bootcamps);
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
