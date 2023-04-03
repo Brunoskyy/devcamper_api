74d246e0b079986cc60f7bde0ded2c87de03414b
Bruno <arturbrunoferreira@gmail.com>
2023-04-03 10:17:51 -0300

    feat: Change env path and improve server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import colors from "colors";
 
 import bootcamps from "./routes/bootcamps";
+import { connectDB } from "./config/db";
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: "./config.env" });
+
+connectDB();
 
 const app = express();
 
@@ -16,6 +20,22 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(
+    colors.yellow.bold(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+  );
+});
+
+process.on("unhandledRejection", (err: Error, promise) => {
+  console.log(colors.red(`Error: ${err.message}`));
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
+
+process.on("uncaughtException", (err: Error, promise) => {
+  console.log(colors.red(`Error: ${err.message}`));
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });
