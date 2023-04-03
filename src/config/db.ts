d0003b107be3750a260560d79bc49b29041ffe8d
Bruno <arturbrunoferreira@gmail.com>
2023-04-03 10:16:35 -0300

    feat: Create db config

---
+++ src/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+export const connectDB = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined");
+    }
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(colors.cyan.bold(`MongoDB Connected: ${conn.connection.host}`));
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    process.exit(1);
+  }
+};
